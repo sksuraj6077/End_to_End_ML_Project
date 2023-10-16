e525e27d0d528d711f320c8523f9f89c6cfcabe8
sksuraj6077 <surajkawathale0122@gmail.com>
2023-10-16 13:34:23 +0530

    edit app.py

--- app.py
+++ app.py
@@ -37,4 +37,8 @@ def predict_datapoint():
         results=round(pred[0],2)
 
         return render_template('results.html',final_result=results)
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0',port=8080)
     
